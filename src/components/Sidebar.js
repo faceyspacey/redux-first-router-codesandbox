6a51e5237c5d6fcd9d0b4b20b20f75aa32c30091
James Gillmore <james@faceyspacey.com>
2017-07-09 16:54:36 -0700

    move ADMIN button up a bit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -50,7 +50,7 @@ const Sidebar = ({ onClick, path }) =>
       FP
     </span>
 
-    <div style={{height: 20 }} />
+    <div style={{height: 17 }} />
 
     <Link to={{ type: 'ADMIN' }}>ADMIN</Link>
   </div>
